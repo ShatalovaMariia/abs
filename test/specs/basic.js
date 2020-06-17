b409e37e67c5ce0a64f7066f9ae3dc617b9d408f
ShatalovaMariia <shatomasho@gmail.com>
2020-06-17 13:16:12 -0700

    add new test

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,16 +1,26 @@
+ import {expect} from 'chai';
+// const {expect} = require('chai');
+//const expect = require('chai').expect
+
+
 describe('Main Page', () =>{
     it('should title Progress Monitor present', function () {
         browser.url('https://stage.pasv.us/' )
         const mainTitle = $('[id="site-name"]').getText()
-        expect(mainTitle).toEqual('Progress Monitor')
+        expect(mainTitle).eq('Progress Monitor')
 
     });
     it('should  title on main page is clickable', function () {
-        expect($('[id="site-name"]').isClickable()).toEqual(true);
+        expect($('[id="site-name"]').isClickable()).eq(true);
 
 
     });
     it('should login button present', function () {
-        expect($('[qa="login-link"]').getText()).toEqual('Login')
+        expect($('[qa="login-link"]').getText()).eq('Login')
     });
+    it('should verify login button is clickable', function () {
+        expect($('[qa="login-link"]').isClickable()).eq(true)
+        
+    });
+
 })
