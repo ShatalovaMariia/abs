441c3c059497d754724f679eba6441b20a689c82
ShatalovaMariia <shatomasho@gmail.com>
2020-06-26 11:50:14 -0700

    vetka

---
+++ test/vetka.js
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+
+describe('RIGISTRATION PAGE',()=> {
+    before(() => {
+        browser.maximizeWindow()
+        browser.url('https://app.pasv.us/user/register')
+    });
+});
